4aee3c5ec4c1bd0fcf44d287eb77b24cbdc5cdfa
Leonardo <leonardo.mf@ufu.br>
2024-01-29 19:51:26 -0300

    versao 1 ok

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # beatmap_downloader
-Script python para para realizar downloads do beatconnect io a partir de uma lista com os id`s dos beatmaps
+Script python para realizar downloads do beatconnect io a partir de uma lista com os id`s dos beatmaps
---
+++ beatmapDownloader.py
@@ -0,0 +1,57 @@
+'''
+    Já faz o serviço
+    Pode melhorar usando request assincrono
+'''
+
+import requests
+from requests.exceptions import HTTPError
+import os
+
+def request(beatmap):
+    try:
+        filename = f'{beatmap}'
+        print('======================================')
+        print(f'Realizando download do {beatmap}:\n')
+        with requests.get(f'https://beatconnect.io/b/{beatmap}', stream=True) as request:
+            if request.ok:
+                if "content-disposition" in request.headers:
+                    content_disposition = request.headers["content-disposition"]
+                    filename = content_disposition.split("filename=")[1]
+                    filename = filename[1:-2]
+                with open(filename, 'wb') as file:
+                    for chunk in request.iter_content(chunk_size=10 * 1024):
+                        file.write(chunk)
+    except HTTPError as http_err:
+        print(f'HTTP error occurred: {http_err}')
+    except Exception as err:
+        print(f'Other error occurred: {err}')
+    else:
+        print(f'Beatmap: {filename} downloaded')
+
+def find_beatmaps(folder):
+    dirList = os.listdir(folder)
+    beatmaps = []
+    for dir in dirList:
+        name = dir.split()
+        beatmaps.append(name[0])
+    return beatmaps
+
+def save_list(beatmaps):
+    with open('list.txt', 'wt') as listFile:
+        listFile.write('\n'.join(beatmaps))
+
+def read_from(list):
+    print('building!')
+
+def menu():
+    print('building!')
+
+# download
+beatmaps = find_beatmaps(".\Songs")
+for beatmap in beatmaps:
+    request(beatmap)
+print('download concluido.')
+
+''' SALVAR A LISTA
+save_list(beatmaps)
+'''
